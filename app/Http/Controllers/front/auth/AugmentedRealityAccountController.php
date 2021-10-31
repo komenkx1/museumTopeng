4b3747a124bab0b690f77ead059c79a40e6aa2b9
komenkx1 <komangwahyu@student.unud.ac.id>
2021-10-31 20:39:06 +0800

    delete from ip

--- app/Http/Controllers/front/auth/AugmentedRealityAccountController.php
+++ app/Http/Controllers/front/auth/AugmentedRealityAccountController.php
@@ -47,7 +47,7 @@ class AugmentedRealityAccountController extends Controller
     public function logout()
     {
         // dd( Session::getId());
-        $oldSession = SessionModel::where('augmented_reality_accounts_id', Auth::guard('augmentedRealities')->user()->id)->first();
+        $oldSession = SessionModel::where('ip_address',  $this->getIp())->first();
         // dd($oldSession);
         SessionModel::destroy($oldSession->id);
         //    dd(  Auth::guard('augmentedRealities')->user()); 
